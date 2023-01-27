3c2bf2a3d33009e0a99c8ae40dadca3ca069fa91
kotoriShoji <kotorishoji@gmail.com>
2023-01-27 09:14:14 -0800

    setup

--- app/src/main/java/com/example/covidtracker/CountyListActivity.kt
+++ app/src/main/java/com/example/covidtracker/CountyListActivity.kt
@@ -2,10 +2,14 @@ package com.example.covidtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.covidtracker.databinding.ActivityCountyListBinding
 
 class CountyListActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCountyListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityCountyListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
